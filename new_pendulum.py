6903f208e5dbee197386aadb451939e5e515a5d7
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-20 16:05:12 -0500

    Update new_pendulum.py

--- new_pendulum.py
+++ new_pendulum.py
@@ -4,7 +4,7 @@ import math
 def update_system(acc,theta,w,time1,time2):
     # position and velocity update below
     dt = time2-time1
-    thetaNext = theta+w*dt+1/2*acc*dt
+    thetaNext = theta+w*dt
     wNext = w+acc*dt
     return thetaNext,wNext
 
@@ -12,18 +12,22 @@ def print_system(time,theta,w):
     print("TIME:     ", time)
     print("Theta: ", theta)
     print("Angular Velocity: ", w, "\n")
+    print((theta, w))
+
 
 # initial conditions
-theta = [0]
+theta = [-.1745]
 w = [0]
-acc = 9.8*(math.cos((math.pi/2)-theta[-1]))
-time = np.linspace(0,20,21)
+acc = (-1)*9.8*(math.sin((theta[0])))
+time = np.linspace(0,30,210)
 print_system(time[0],theta[0],w[0])
+
 
 i = 1
 while i < len(time):
     # update position and velocity using previous values and time step
-    thetaNext, wNext = update_system(acc,theta[i-1],w[i-1],time[i-1],time[i])
+    acc = (-1)*9.8*(math.sin((theta[i-1])))
+    thetaNext, wNext = update_system(acc, theta[i-1],w[i-1],time[i-1],time[i])
     theta.append(thetaNext)
     w.append(wNext)
     print_system(time[i],theta[i],w[i])
