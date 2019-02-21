2fa35aa15c3eea5f6514e513f97335758e2547d3
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-21 15:47:12 -0500

    Update new_pendulum.py

--- new_pendulum.py
+++ new_pendulum.py
@@ -4,9 +4,10 @@ import math
 def update_system(acc,theta,w,time1,time2):
     # position and velocity update below
     dt = time2-time1
-    thetaNext = theta+w*dt
+    accNext = -9.8/l*(math.sin((theta)))-friction*w
     wNext = w+acc*dt
-    return thetaNext,wNext
+    thetaNext = theta+w*dt+1/2*acc*dt**2
+    return thetaNext,wNext,accNext
 
 def print_system(time,theta,w):
     print("TIME:     ", time)
@@ -16,20 +17,21 @@ def print_system(time,theta,w):
 
 
 # initial conditions
-theta = [-.1745]
+l = 1
+theta = [.2]
 w = [0]
-acc = 9.8*(math.sin((theta[0])))
-time = np.linspace(0,30,210)
+acc = [-9.8/l*(math.sin((theta[0])))]
+time = np.linspace(0,20,10000)
 print_system(time[0],theta[0],w[0])
+friction = .0001
 
 
 i = 1
 while i < len(time):
     # update position and velocity using previous values and time step
-    acc = (-1)*9.8*(math.sin((theta[i-1])))
-    print(acc)
-    thetaNext, wNext = update_system(acc, theta[i-1],w[i-1],time[i-1],time[i])
+    thetaNext, wNext, accNext = update_system(acc[i-1], theta[i-1],w[i-1],time[i-1],time[i])
     theta.append(thetaNext)
     w.append(wNext)
+    acc.append(accNext)
     print_system(time[i],theta[i],w[i])
     i += 1
