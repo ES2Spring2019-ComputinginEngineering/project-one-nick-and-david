2c4cf2702cdb10f7f40228b1167de9983868e956
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-26 15:47:15 -0500

    Update test file.py

--- test file.py
+++ test file.py
@@ -7,8 +7,8 @@ with open('test' + a + '.txt', 'w') as my_file:
         microbit.display.set_pixel(2, 2, 9) #indicator that it is recording
 #        t0 = microbit.running_time()
         microbit.sleep(100)
-  #      t1 = microbit.running_time()
-   #     dt = str(t1-t0)
+#      t1 = microbit.running_time()
+#     dt = str(t1-t0)
         x = str(microbit.accelerometer.get_values())
         my_file.write(x + '\r\n')
 microbit.display.set_pixel(2, 2, 0) #not recording
