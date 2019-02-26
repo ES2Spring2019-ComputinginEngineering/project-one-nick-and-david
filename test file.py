6ba53332bc3077b5a1931af3e6963fed27ee0c0c
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-26 15:44:10 -0500

    Update test file.py

--- test file.py
+++ test file.py
@@ -1,7 +1,13 @@
 import microbit
-
+import random
+a = random.randint(0, 1000)
 with open('datatest.txt', 'w') as my_file:
-    for i in range(20):
-        #x = str(microbit.accelerometer.get_values())
-        my_file.write('hello\r\n')
-        microbit.sleep(500)
+    for i in range(200):
+        microbit.display.set_pixel(2, 2, 9) #indicator that it is recording
+#        t0 = microbit.running_time()
+        microbit.sleep(100)
+  #      t1 = microbit.running_time()
+   #     dt = str(t1-t0)
+        x = str(microbit.accelerometer.get_values())
+        my_file.write(x + '\r\n')
+microbit.display.set_pixel(2, 2, 0) #not recording
