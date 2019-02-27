e3f93abcb2c778de188b98b74847057bdc192a46
nsragusa <nicolas.ragusa@tufts.edu>
2019-02-27 16:02:30 -0500

    Update Pendulum_Data_Collection.py

--- Pendulum_Data_Collection.py
+++ Pendulum_Data_Collection.py
@@ -2,14 +2,16 @@ import microbit
 import random
 
 a = str(random.randint(1, 100))
-
 while True:
     if microbit.button_a.was_pressed() == True:                         # waits for button_a pressed
         microbit.sleep(5000)                                            # 3 second delay
+        time0 = microbit.running_time()
         with open('real_pendulum_data' + a + '.txt', 'w') as my_file:   # makes file 1 to 100
             for i in range(400):                                        # 400 * .025 seconds = 10 seconds (close!)
                 microbit.display.set_pixel(2, 2, 9)                     # indicator that it is recording
                 microbit.sleep(25)                                      # sleep at 25 milliseconds
                 y = str(microbit.accelerometer.get_y())                 # writes data in folder (only y)
-                my_file.write(y + '\n')                                 # new lines
+                time1 = microbit.running_time()
+                elapsed_time = time1-time0
+                my_file.write(elapsed_time + ', ' + y + '\n')           # new lines
     microbit.display.set_pixel(2, 2, 0)                                 # not recording
