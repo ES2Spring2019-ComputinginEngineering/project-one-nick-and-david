82484e3e226b37fe7273cd7a15e1385b1fb24549
nsragusa <46976298+nsragusa@users.noreply.github.com>
2019-03-04 15:46:02 -0500

    Update Pendulum_Data_Collection.py

--- Pendulum_Data_Collection.py
+++ Pendulum_Data_Collection.py
@@ -3,15 +3,15 @@ import random
 
 a = str(random.randint(1, 100))
 while True:
-    if microbit.button_b.was_pressed() == True:                         # waits for button_a pressed
-        microbit.sleep(5000)                                            # 3 second delay
+    if microbit.button_b.was_pressed() == True:                         # waits for button_b pressed
+        microbit.sleep(5000)                                            # 5 second delay
         time0 = microbit.running_time()
-        with open('real_pendulum_data' + a + '.txt', 'w') as my_file:   # makes file 1 to 100
+        with open('real_pendulum_data' + a + '.txt', 'w') as my_file:   # makes file named 1 to 100
             for i in range(400):                                        # 400 * .025 seconds = 10 seconds (close!)
-                microbit.display.set_pixel(2, 2, 9)                     # indicator that it is recording
+                microbit.display.set_pixel(2, 2, 9)                     # indicator that it is recording data
                 microbit.sleep(25)                                      # sleep at 25 milliseconds
                 y = str(microbit.accelerometer.get_y())                 # writes data in folder (only y)
                 time1 = microbit.running_time()
-                elapsed_time = str(time1-time0)
-                my_file.write(elapsed_time + ', ' + y + '\n')           # new lines
-    microbit.display.set_pixel(2, 2, 0)                                 # not recording
+                elapsed_time = str(time1-time0)                         # gets the elapsed time from the last data write
+                my_file.write(elapsed_time + ', ' + y + '\n')           # writes data, makes new lines
+    microbit.display.set_pixel(2, 2, 0)                                 # no longer recording
