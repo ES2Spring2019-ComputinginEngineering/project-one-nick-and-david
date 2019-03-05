205e5e002e4eca18c97a968f0cd9407acebd416b
nsragusa <46976298+nsragusa@users.noreply.github.com>
2019-03-05 16:15:18 -0500

    Update Spyder_Data_Reader.py

--- Spyder_Data_Reader.py
+++ Spyder_Data_Reader.py
@@ -1,6 +1,7 @@
-#
+#Real-World Data Analysis
 #Contributers: David Fricke and Nicolas Ragusa
-#
+#Takes real world pendulum data and provides a original and filtered graph of both theta vs time and angular acceleration vs time
+#and provides the average period of oscillation
 
 
 import math
@@ -40,7 +41,7 @@ time_of_pks = []
 theta_pks = []
 x=0
 
-for i in range(len(filt_theta_pks)):
+for i in range(len(filt_theta_pks)):           #makes x and y values to plot the location of peaks later after filtering
     z = filt_theta_pks[x]
     theta_pks.append(theta_list_filt[z])
     b = time_list[z]
@@ -53,13 +54,13 @@ avg_period = []
 average = 0
 
 i = 1
-while i < len(time_of_pks):
+while i < len(time_of_pks):                             #filters out peaks that are too close (identifying the same peak)
     if abs(time_of_pks[i-1] - time_of_pks[i]) > .75:
         new_period.append(time_of_pks[i])
     i = i + 1
     
 n = 1
-while n < len(new_period):
+while n < len(new_period):                              #finds time between neighboring peaks
     x = abs(new_period[n-1] - new_period[n])
     avg_period.append(x)
     n = n + 1
@@ -67,11 +68,11 @@ while n < len(new_period):
 for item in avg_period:
     average += item
 
-print('Average period:',round(average/len(avg_period),2),'sec')
+print('Average period:',round(average/len(avg_period),2),'sec')     #prints a calculated average of period
 
 plt.figure(figsize=(8,10))
 
-plt.subplot(4,1,1)
+plt.subplot(4,1,1)                              #plots theta vs time
 plt.plot(time_list, theta_list, 'ro--')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Theta (Degrees)')
@@ -79,7 +80,7 @@ plt.title('Original Theta vs Time')
 plt.xlim((0, 14))
 plt.grid()
 
-plt.subplot(4,1,2)
+plt.subplot(4,1,2)                              #plots filtered thetas vs time
 plt.plot(time_list, theta_list_filt, 'ro--', time_of_pks, theta_pks, 'b.') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Theta (Degrees)')
@@ -87,7 +88,7 @@ plt.title('Median Filtered Theta vs Time')
 plt.xlim((0, 14))
 plt.grid()
 
-plt.subplot(4,1,3)
+plt.subplot(4,1,3)                               #plots angular acceleration vs time
 plt.plot(time_list, angular_acc_list, 'ro--') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angular Acc (Degrees/s^2)')
@@ -95,7 +96,7 @@ plt.title('Angular vs Time')
 plt.xlim((0, 14))
 plt.grid()
 
-plt.subplot(4,1,4)
+plt.subplot(4,1,4)                                #plots filtered angular acceleration vs time
 plt.plot(time_list, angular_acc_list_filt, 'ro--') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angular Acc (Degrees/s^2)')
