f06cfd88d7a5066f270df37778f67414eccce9ea
nsragusa <46976298+nsragusa@users.noreply.github.com>
2019-03-05 15:21:37 -0500

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -5,14 +5,18 @@ Created on Mon Mar  4 20:55:54 2019
 
 @author: DRFricke
 """
+#Pendulum data analysis code
+#Contributers: David Fricke and Nicolas Ragusa
+#This code takes manually inputted pendulum lengths and periods (from simulated and real-world data)
+#and provides a graph comparing the two
+
 import matplotlib.pyplot as plt
 
-length = [0.447, 0.413, 0.346, 0.298, 0.225]
+length = [0.447, 0.413, 0.346, 0.298, 0.225]          #manually inputted data (can be changed)
 period_actual = [1.26, 1.2, 1.07, 0.99, 0.85]
 period_sim = [1.07, 1.03, 0.97, 0.91, 0.82]
 
-plt.figure(figsize=(5,5))
-
+plt.figure(figsize=(5,5))                             #sets up graph to display manually inputted data
 plt.subplot(1,1,1)
 plt.plot(length, period_actual, 'bs', length, period_sim, 'r^')
 plt.annotate('Triangle = Simulated Data', xy=(0.27, 1.2))
