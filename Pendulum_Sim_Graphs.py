c8e6bf194fc8146f8c94847078ca3976a941aaac
nsragusa <46976298+nsragusa@users.noreply.github.com>
2019-03-05 16:02:11 -0500

    Update Pendulum_Sim_Graphs.py

--- Pendulum_Sim_Graphs.py
+++ Pendulum_Sim_Graphs.py
@@ -23,7 +23,6 @@ theta = [.09]
 w = [0]
 acc = [-9.8/l*(math.sin(theta[0]))]
 time = np.linspace(0,14,15000)
-#print_system(time[0],theta[0],w[0])
 friction = .1
 
 
@@ -33,16 +32,14 @@ while i < len(time):        # update position, velocity, and acceleration using 
     theta.append(thetaNext)
     w.append(wNext)
     acc.append(accNext)
-    #print_system(time[i],theta[i],w[i])
     i += 1
     
-short_theta = np.array(theta[::20])
+short_theta = np.array(theta[::20])             #finds peaks, filters out so only every 20th point is considered
 short_time = time[::20]   
-# Find peaks of all waves (started)
 theta_pks, _ = sig.find_peaks(short_theta)
 acc_pks, _ = sig.find_peaks(acc)
 
-time_of_pks = short_time[theta_pks]
+time_of_pks = short_time[theta_pks]             #makes x and y values so the peaks can be graphed
 short_theta_pks = short_theta[theta_pks]
 
 new_period = []
@@ -50,13 +47,13 @@ avg_period = []
 average = 0
 
 i = 1
-while i < len(time_of_pks):
+while i < len(time_of_pks):                                 #filters out peaks that are too close together
     if abs(time_of_pks[i-1] - time_of_pks[i]) > .75:
         new_period.append(time_of_pks[i])
     i = i + 1
     
 n = 1
-while n < len(new_period):
+while n < len(new_period):                              #finds time between peaks
     x = abs(new_period[n-1] - new_period[n])
     avg_period.append(x)
     n = n + 1
